59ff02daa6702c8540dadb717fc2ef3830925693
Yaroslav <kprogrammer0pr@gmail.com>
2023-01-01 17:28:53 +0200

    Add Github-pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/reduxStore";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -10,11 +10,11 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
