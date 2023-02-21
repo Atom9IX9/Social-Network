35099743407367ef779550b049e75fb9ced11f01
Yaroslav <kprogrammer0pr@gmail.com>
2023-02-21 22:33:56 +0200

    load logo

--- src/components/Navbar/Navigationbar.tsx
+++ src/components/Navbar/Navigationbar.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Link from "./Link/Link";
+import logo from "../../assets/img/logo.jpg";
 import Sider from "antd/es/layout/Sider";
 import {
   UserOutlined,
@@ -8,57 +9,62 @@ import {
   SettingOutlined,
   FileSearchOutlined,
   CommentOutlined,
-  MessageOutlined
+  MessageOutlined,
 } from "@ant-design/icons";
 import { Menu } from "antd";
 
 const Navbar: React.FC<TNavbarProps> = ({ collapsed }) => {
   return (
     <Sider trigger={null} collapsible collapsed={collapsed}>
-          <div className="logo">logo</div>
-          <Menu
-            theme="dark"
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            items={[
-              {
-                key: "1",
-                icon: <UserOutlined />,
-                label: <Link linkName="Profile" path="profile" />,
-              },
-              {
-                key: "2",
-                icon: <MessageOutlined />,
-                label: <Link linkName="Messages" path="messages" />,
-              },
-              {
-                key: "3",
-                icon: <FileSearchOutlined />,
-                label: <Link linkName="Users" path="users" />,
-              },
-              {
-                key: "4",
-                icon: <CommentOutlined />,
-                label: <Link linkName="Chat" path="chat" />
-              },
-              {
-                key: "5",
-                icon: <ExceptionOutlined />,
-                label: <Link linkName="News" path="news" />,
-              },
-              {
-                key: "6",
-                icon: <CustomerServiceOutlined />,
-                label: <Link linkName="Music" path="music" />,
-              },
-              {
-                key: "7",
-                icon: <SettingOutlined />,
-                label: <Link linkName="Settings" path="settings" />,
-              },
-            ]}
-          />
-        </Sider>
+      <div
+        className="logo"
+        style={{ display: "flex", justifyContent: "space-around" }}
+      >
+        <img style={{ width: "40%" }} src={logo} alt="logo" />
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["1"]}
+        items={[
+          {
+            key: "1",
+            icon: <UserOutlined />,
+            label: <Link linkName="Profile" path="profile" />,
+          },
+          {
+            key: "2",
+            icon: <MessageOutlined />,
+            label: <Link linkName="Messages" path="messages" />,
+          },
+          {
+            key: "3",
+            icon: <FileSearchOutlined />,
+            label: <Link linkName="Users" path="users" />,
+          },
+          {
+            key: "4",
+            icon: <CommentOutlined />,
+            label: <Link linkName="Chat" path="chat" />,
+          },
+          {
+            key: "5",
+            icon: <ExceptionOutlined />,
+            label: <Link linkName="News" path="news" />,
+          },
+          {
+            key: "6",
+            icon: <CustomerServiceOutlined />,
+            label: <Link linkName="Music" path="music" />,
+          },
+          {
+            key: "7",
+            icon: <SettingOutlined />,
+            label: <Link linkName="Settings" path="settings" />,
+          },
+        ]}
+      />
+    </Sider>
   );
 };
 
